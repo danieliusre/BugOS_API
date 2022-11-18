a25d8c25fbd43728181093b2a4ac6a67cd4f72a3
Paulina <78165114+paulinaa002@users.noreply.github.com>
2022-11-18 18:27:34 +0200

    changing context

--- Models/POSContext.cs
+++ Models/POSContext.cs
@@ -9,14 +9,21 @@ namespace POSApi.Models
         {
         }
 
+        public DbSet<Address> Addresses { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Discount> Discounts { get; set; }
         public DbSet<Employee> Employees { get; set; }
         public DbSet<EmployeePermissions> EmployeePermissions { get; set; }
         public DbSet<EventHistory> EventHistories { get; set; }
+        public DbSet<GiftCode> GiftCodes { get; set; }
         public DbSet<Incident> Incidents { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<OrderPayment> OrderPayments { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Shift> Shifts { get; set; }
+        public DbSet<Tax> Taxes { get; set; }
     }
 }
