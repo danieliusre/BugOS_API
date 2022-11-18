1c9796675d96b91bde5cd4cbc19a87d4ced02e36
Paulina <78165114+paulinaa002@users.noreply.github.com>
2022-11-18 17:33:19 +0200

    incident

---
+++ Models/Incident.cs
@@ -0,0 +1,10 @@
+namespace IncidentApi.Models
+{
+    public class Incident
+    {
+        public ulong Id {get; set;}
+        public ulong Order {get; set;}
+        public ulong Customer {get; set;}
+        public ulong Emploee {get; set;}
+    }
+}
