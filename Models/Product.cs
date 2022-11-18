13efdcde8d7205f6cb07dea72ea33e956acfa79d
Paulina <78165114+paulinaa002@users.noreply.github.com>
2022-11-18 16:44:51 +0200

    adding product model

---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+namespace TodoApi.Models
+{
+    public class Product
+    {
+        public ulong Id {get; set;}
+        public string Name {get; set;}
+        public string Description {get; set;}
+        public decimal Price {get; set;}
+        public uint Stock {get; set;}
+        public string Barcode {get; set;}
+        public ulong Category {get; set;}
+        public ulong Discount {get; set;}
+    }
+}
